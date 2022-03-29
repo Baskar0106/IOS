ccf26b40812ea43686c6e62aa821d3cac4169643
Bhaskar Venkata Prudhvi Bobbala <89476750+Baskar0106@users.noreply.github.com>
2022-03-29 10:48:45 -0500

    Add files via upload

---
+++ TableViewDemo/TableViewDemo/DetailsViewController.swift
@@ -0,0 +1,33 @@
+//
+//  DetailsViewController.swift
+//  TableViewDemo
+//
+//  Created by Bobbala,Bhaskar Venkata Prudhvi on 3/29/22.
+//
+
+import UIKit
+
+class DetailsViewController: UIViewController {
+    @IBOutlet weak var displayOutlet: UILabel!
+    
+    var product : Product?
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        displayOutlet.text = "The Product \((product?.productName)!) is of \((product?.productCategory)!) category "
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
